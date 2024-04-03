7ee345f84d5a5f58778a532969349249bac63129
Moisesmp75 <moisesmore75@gmail.com>
2024-04-03 12:48:50 -0500

    add dotenv

--- src/database/db_connection.go
+++ src/database/db_connection.go
@@ -18,7 +18,8 @@ func InitDatabase() {
 	password := os.Getenv("DB_PASSWORD")
 	port := os.Getenv("DB_PORT")
 	db_name := os.Getenv("DB_NAME")
-	MYSQL := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, port, db_name)
+	host := os.Getenv("DB_HOST")
+	MYSQL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, db_name)
 	var err error
 	DB, err = gorm.Open(mysql.Open(MYSQL), &gorm.Config{})
 	if err != nil {
